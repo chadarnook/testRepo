7e96dcba53c0de68ebca3e8e52ed8a37b82b22fa
acali843 <acali843@HDQBEPC261223.mihdq.marrcorp.marriott.com>
2015-06-24 14:46:33 -0400

    Added env properties

---
+++ rest-dsl-camel-example/src/main/java/com/accenture/conf/Configuration.java
@@ -0,0 +1,17 @@
+package com.accenture.conf;
+
+import org.springframework.stereotype.Component;
+
+public class Configuration {
+	
+	private String property;
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+
+}
--- rest-dsl-camel-example/src/test/java/com/accenture/routes/RoutesTest.java
+++ rest-dsl-camel-example/src/test/java/com/accenture/routes/RoutesTest.java
@@ -2,7 +2,8 @@ package com.accenture.routes;
 
 import javax.ws.rs.core.Response;
 
-import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -10,10 +11,23 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
-public class RoutesTest extends CamelTestSupport{
-	
+import com.accenture.conf.Configuration;
+
+@RunWith(CamelSpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/applicationContext.xml")
+@ActiveProfiles("unittest")
+public class RoutesTest extends CamelSpringTestSupport{
 	private Server server;
+	
+	@Autowired
+	Configuration conf;
 	
 	@Before
 	public void startServer() throws Exception {
@@ -32,10 +46,10 @@ public class RoutesTest extends CamelTestSupport{
 		server.stop();
 	}
 
-//	@Override
-//	protected AbstractApplicationContext createApplicationContext() {
-//		return new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/applicationContext.xml");
-//	}
+	@Override
+	protected AbstractApplicationContext createApplicationContext() {
+		return new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/applicationContext.xml");
+	}
 	
 //	@Override
 //	protected RouteBuilder createRouteBuilder() {
@@ -62,6 +76,7 @@ public class RoutesTest extends CamelTestSupport{
 //	}
 	@Test
 	public void testRoutes() throws InterruptedException {
+		System.out.println("--------------------- " + conf.getProperty());
 //		WebClient client = WebClient.create("http://localhost:8080/test/route");
 		WebClient client = WebClient.create("http://localhost:8080/rest-dsl-camel-example/rest/test/route").header("hotelId", 1);
 		Response res = client.get();
@@ -69,6 +84,5 @@ public class RoutesTest extends CamelTestSupport{
 //		String res = template.requestBody("http://localhost:8080/test/route", null, String.class);
 //		Assert.assertEquals("200", res);
 	}
-	
 
 }
